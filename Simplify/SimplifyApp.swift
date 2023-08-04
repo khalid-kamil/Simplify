623dc4aa2e30f89c4ca4681acef0a420b67fbe52
Khalid Kamil <khalidkamil28@gmail.com>
2023-08-04 20:15:47 +0100

    Initial Commit

---
+++ Simplify/SimplifyApp.swift
@@ -0,0 +1,20 @@
+//
+//  SimplifyApp.swift
+//  Simplify
+//
+//  Created by Khalid Kamil on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimplifyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
